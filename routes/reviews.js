5be2ac4f71c89468d70e0256799d96803ba57089
sruthiravindra <sruthi.ravindran@gmail.com>
2023-10-12 10:16:52 +0400

    working on reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const reviewRouter = express.Router();
 const Review = require('../models/review');
+const authenticate = require('../authenticate');
+const cors = require('./cors');
+const ObjectId = require('mongodb').ObjectId;
 
-reviewRouter.route('/')
-.get((req, res, next) => {
+reviewRouter.options('*', cors.corsWithOptions, (req, res) => {
+    res.sendStatus(200);
+  });
+
+// changed from get to post because get cannot send data in body and here we need to send filter parameters
+reviewRouter.route('/fetchReviews')
+.post(cors.corsWithOptions,(req, res, next) => {
     const {filter_author_id, filter_reviewed_user_id} = req.body;
+    console.log(req.body, filter_author_id,filter_reviewed_user_id);
     if (filter_author_id) {
-        const query = { author_id: ObjectId(filter_author_id) }
+        const query = { author_id: new ObjectId(filter_author_id) }
         Review.find(query)
         .then(reviews => {
             res.status = 200;
@@ -15,7 +24,7 @@ reviewRouter.route('/')
         })
         .catch(err => next(err));
     } else if (filter_reviewed_user_id) {
-        const query = { reviewed_user_id: ObjectId(filter_reviewed_user_id)};
+        const query = { reviewed_user_id: new ObjectId(filter_reviewed_user_id)};
         Review.find(query)
         .then(reviews => {
             res.status = 200;
@@ -33,7 +42,7 @@ reviewRouter.route('/')
         .catch(err => next(err));
     }
 })
-.post((req, res, next) => {
+.post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
     //need to verify current user is logged in
     //need to check if reviewed_user_id exists in author_id's contacts
     //moderator?
