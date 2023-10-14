c703d8b9fbd0e90d16f9292e4af7ee56e8115b08
effieguenther <effiegguenther@gmail.com>
2023-10-14 18:08:27 +0545

    minor bug fixes and fine tuning

--- models/profile.js
+++ models/profile.js
@@ -44,7 +44,7 @@ const profileSchema = new Schema({
     },
     address: addressSchema,
     services: [{ type: Schema.Types.ObjectId, ref: "Service" }],
-    contacts: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    contacts: [{ type: Schema.Types.ObjectId, ref: "Profile" }],
     images: [{ type: String }],
     profile_pic: {type: String, default: "https://firebasestorage.googleapis.com/v0/b/wordofmouth-alpha.appspot.com/o/profile-pictures%2Fprofile-default.png?alt=media&token=ee3c6a06-eefa-4bed-8e4a-d0278c9f33fb&_gl=1*1ooivdz*_ga*MTAyODI4NzM3NC4xNjkwNTQyNTYy*_ga_CW55HF8NVT*MTY5NjY1MTM0NS42OS4xLjE2OTY2NTMwNzMuNjAuMC4w"},
     email: {type: String, default: "" },
